85204e9ac3792e190d645c9f1a258cbebc2e19c7
welsongds <132483637+welsongds@users.noreply.github.com>
2023-09-04 11:21:26 -0300

    Exercício 1 - Resolvido
    Este é o exercício 1 da Atividade verificadora de aprendizagem

---
+++ contagem.py
@@ -0,0 +1,7 @@
+def contando_ate(min, max):
+    with open("crescente.txt", "w", encoding="UTF-8") as arquivo:
+        for elemento in range(min, max + 1):
+            arquivo.write(str(elemento) + ";")
+
+
+contando_ate(1, 100)
